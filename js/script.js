a9da71918f4185a910d52be79b6e0a512203d9ab
jrcosta23 <141432782+jrcosta23@users.noreply.github.com>
2023-09-20 11:57:33 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function enviarMensagem(){
+     var nome = document.getElementById("idNome");
+
+     if(nome.value != "") {
+         alert("obrigado,sr(a)" + nome.value +", sua mensagem foi enviada com sucesso");
+
+
+     }
+}
+
+
+
